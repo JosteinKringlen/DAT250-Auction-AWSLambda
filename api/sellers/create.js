255853c98790a1fad8186b778a2f84ca337877a4
Jostein Kringlen <josteinkringlen@gmail.com>
2017-11-20 18:17:12 +0100

    Added API for creating sellers

--- api/sellers/create.js
+++ api/sellers/create.js
@@ -9,6 +9,64 @@ AWS.config.setPromisesDependency(require('bluebird'));
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
+    const bodyData = JSON.parse(event.body);
+    const name = bodyData.seller_name;
+    const email = bodyData.seller_email;
+    const address = bodyData.seller_adress;
+    const phonenumber = bodyData.seller_phone;
 
 
+    if(typeof name !== 'string' || typeof title !== 'string' || typeof description !== 'string'
+        || typeof startingPrice !== 'number'){
+        console.error('Bad stuff');
+        callback(null, {
+            statusCode: 400,
+            headers: {'Content-Type': 'text/plain'},
+            body: 'Bad stuff happened. Sry',
+        });
+    }
+
+    submitSeller(sellerParams(name, email, address, phonenumber))
+        .then(res => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'Good stuff',
+                    title: res.title
+                })
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            callback(null, {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: 'Bad stuff when creating user'
+                })
+            })
+        });
+};
+
+const submitSeller = seller => {
+    console.log('Submitting auction');
+    const auctionInfo = {
+        TableName: process.env.SELLER_TABLE,
+        Item: seller,
+    };
+    return dynamoDb.put(auctionInfo).promise()
+        .then(res => seller);
+};
+
+const sellerParams = (name, email, address, phonenumber) => {
+    const timestamp = new Date().getTime();
+
+    return {
+        uuid: uuid.v1(),
+        name: name,
+        email: email,
+        address: address,
+        phonenumber: phonenumber,
+        createdAt: timestamp,
+        updatedAt: timestamp
+    }
 };
