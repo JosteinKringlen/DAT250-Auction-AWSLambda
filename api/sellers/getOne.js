7e29d22c7a15688b1d9031a8c89f3c24fbf93db9
Jostein Kringlen <josteinkringlen@gmail.com>
2017-11-20 20:17:58 +0100

    Added API endpoint for getting single seller

---
+++ api/sellers/getOne.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: "eu-west-2" });
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+    const params = {
+        TableName: process.env.SELLER_TABLE,
+        Key: {
+            seller_id: event.pathParameters.seller_id
+        },
+    };
+
+    dynamoDb.get(params).promise().then(res => {
+        const result = {
+            statusCode: 200,
+            body: JSON.stringify(res.Item),
+        };
+        callback(null, result);
+    }).catch(err => {
+        console.error(err);
+        callback(new Error('Could not fetch auction.'));
+    });
+};
