add962381a78c21187653ddcd749bbc67e387d4d
Jostein Kringlen <josteinkringlen@gmail.com>
2017-11-21 14:20:56 +0100

    One can now get a seller/user's bids

--- api/sellers/bids.js
+++ api/sellers/bids.js
@@ -3,63 +3,50 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 
-AWS.config.update({ region: "eu-west-2" });
+AWS.config.update({region: "eu-west-2"});
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.bids = (event, context, callback) => {
+
+    const _seller_id = event.pathParameters.seller_id;
+
     let auctionParams = {
         TableName: process.env.AUCTION_TABLE,
         ProjectionExpression: "auction_id, bids",
     };
 
-   /* let sellerParams = {
-        TableName: process.env.SELLER_TABLE,
-        Key: {
-            seller_id: event.pathParameters.seller_id,
-        }
-    };*/
-
     console.log('Getting the stuff');
-    /*const onScan = (err, data) => {
+    let auctions = {};
+    let sellerAuctions = [];
+    const onScan = (err, data) => {
         if (err) {
             console.log('Bad stuff: ', JSON.stringify(err, null, 2));
             callback(err);
         } else {
-            console.log('Good stuff');
+            auctions = data.Items;
+            auctions.forEach(function (item) {
+                if (item.bids.length >=1 ){
+                    for (let i = 0; i < item.bids.length; i++){
+                        if (item.bids[i].bidder_id === _seller_id){
+                            sellerAuctions.push({
+                                auction_id: item.auction_id,
+                                seller_bid: item.bids[i].newBid
+                            })
+                        }
+                    }
+                }
+            });
+            sellerAuctions.forEach(function (item) {
+               console.log(item);
+            });
             return callback(null, {
-                statusCode: 200,
-                body: JSON.stringify({
-                    auctions: data.Items
-                })
+               statusCode: 200,
+               body: sellerAuctions
             })
         }
     };
-    */
-    let auctions = {};
-    const onScan = (err, data) => {
-        if(err){
-            console.log('Bad stuff: ', JSON.stringify(err, null, 2));
-            callback(err);
-        } else {
-             auctions = {
-                 stuff: data.Items,
-             };
-             console.log(auctions.stuff);
-        }
-    };
-
-    /*dynamoDb.get(params).promise().then(res => {
-        const result = {
-            statusCode: 200,
-            body: JSON.stringify(res.Item)
-        };
-        callback(null,result);
-    }).catch(err => {
-        console.log(err);
-        callback(null, err, 2);
-    })*/
     dynamoDb.scan(auctionParams, onScan);
 
 };
