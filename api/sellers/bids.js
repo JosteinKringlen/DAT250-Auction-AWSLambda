c5e94da8a0f638ebb60f1f9c36be504fdf6685e4
Jostein Kringlen <josteinkringlen@gmail.com>
2017-11-21 12:25:33 +0100

    Stuff

--- api/sellers/bids.js
+++ api/sellers/bids.js
@@ -9,6 +9,57 @@ AWS.config.setPromisesDependency(require('bluebird'));
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.bids = (event, context, callback) => {
+    let auctionParams = {
+        TableName: process.env.AUCTION_TABLE,
+        ProjectionExpression: "auction_id, bids",
+    };
 
+   /* let sellerParams = {
+        TableName: process.env.SELLER_TABLE,
+        Key: {
+            seller_id: event.pathParameters.seller_id,
+        }
+    };*/
+
+    console.log('Getting the stuff');
+    /*const onScan = (err, data) => {
+        if (err) {
+            console.log('Bad stuff: ', JSON.stringify(err, null, 2));
+            callback(err);
+        } else {
+            console.log('Good stuff');
+            return callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    auctions: data.Items
+                })
+            })
+        }
+    };
+    */
+    let auctions = {};
+    const onScan = (err, data) => {
+        if(err){
+            console.log('Bad stuff: ', JSON.stringify(err, null, 2));
+            callback(err);
+        } else {
+             auctions = {
+                 stuff: data.Items,
+             };
+             console.log(auctions.stuff);
+        }
+    };
+
+    /*dynamoDb.get(params).promise().then(res => {
+        const result = {
+            statusCode: 200,
+            body: JSON.stringify(res.Item)
+        };
+        callback(null,result);
+    }).catch(err => {
+        console.log(err);
+        callback(null, err, 2);
+    })*/
+    dynamoDb.scan(auctionParams, onScan);
 
 };
